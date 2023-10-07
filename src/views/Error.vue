eb9d3a96cb524350182ce22bdee83b22e78fcd7b
Cyril Tiger <cyriltiger@gmail.com>
2023-10-07 14:31:08 +0200

    set routing + default components

---
+++ src/views/Error.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppHeader from '@/modules/common/Header.vue';
+</script>
+
+<template>
+  <AppHeader />
+  <div>This is the error</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppHeader from '@/modules/common/Header.vue';
+</script>
+
+<template>
+  <AppHeader />
+  <div>This is the Home</div>
+</template>
+
+<style scoped></style>
