5b3ff7a872bf74866f618f1d5ade3d7362ca150f
Cyril Tiger <cyriltiger@gmail.com>
2023-10-14 17:32:12 +0200

    add navigation guard to /my

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Home, Dashboard, Login, Register, Error, Public } from '@/views';
 import { createRouter, createWebHistory } from 'vue-router';
+import { useUserStore } from './store';
+import { isNil } from './helpers/functions';
 
 export const routes = [];
 
@@ -9,7 +11,17 @@ export default createRouter({
     { path: '/', component: Home },
     { path: '/login', component: Login },
     { path: '/register', component: Register },
-    { path: '/my', component: Dashboard }, // this will need to be guarded, redirected to login
+    {
+      path: '/my',
+      component: Dashboard,
+      beforeEnter: () => {
+        const store = useUserStore();
+
+        if (isNil(store.user)) {
+          return { path: '/login' };
+        }
+      },
+    },
     { path: '/public', component: Public }, // this will need a dynamic param for the username
     { path: '/:pathMatch(.*)*', component: Error },
   ],
